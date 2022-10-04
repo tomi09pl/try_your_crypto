fce15baf85aaaeed04825542e334fc3c2d594a99
tomi09pl <tomi09pl@gmail.com>
2022-10-04 22:03:05 +0100

    setting up scss

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Header extends React.Component{
+
+    render () {
+        return (
+            <div className="header_wrapper">
+                <h1 className="header_title">try your crypto</h1>
+            </div>
+        )
+    }
+}
